0d6c874421a675c607257dd4a69f7bd5de8c8a9b
dan-nl <d_entous@yahoo.com>
2016-07-02 05:02:29 +0200

    added stack trace to error logging when not a 404

--- src/error-logger.js
+++ src/error-logger.js
@@ -5,27 +5,19 @@
  * @param {number} err.status
  *
  * @param {IncomingMessage} req
- * @param {string}          req.body
- * @param {Object}          req.connection
- * @param {Object}          req.headers
- * @param {string}          req.method
- * @param {string}          req.originalUrl
- * @param {Object}          req.session
+ * @param {Object} req.body
+ * @param {Object} req.connection
+ * @param {string} req.connection.remoteAddress
+ * @param {Object} req.headers
+ * @param {string} req.method
+ * @param {string} req.originalUrl
+ * @param {Object} req.session
+ *
+ * @param {ServerResponse} [res]
  */
-module.exports = function errorLogger( err, req ) {
-  var msg;
+module.exports = function errorLogger( err, req, res ) {
+  var msg = { error: {} };
 
-  if ( !( err instanceof Error ) ) {
-    console.error( 'errorLogger( ' + err + ', ' +  req + ' ) err not provided as an Error' );
-    return;
-  }
-
-  if ( !( req instanceof Object ) ) {
-    console.error( 'errorLogger( ' + err + ', ' +  req + ' ) req not provided as an Object' );
-    return;
-  }
-
-  msg = { error: {} };
   msg.error.method = req.method || null;
   msg.error.originalUrl = req.originalUrl || null;
   msg.error.status = err.status || 500;
@@ -37,4 +29,8 @@ module.exports = function errorLogger( err, req ) {
   msg.error.err = err || null;
 
   console.error( msg );
+
+  if ( err.status !== 404 ) {
+    console.error( err.stack );
+  }
 };
